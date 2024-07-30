94e0b6dd551d7f85c99987ec53793f3a9e15a36c
Akere Aishat <mojiradeaisha850@gmail.com>
2024-07-30 01:26:38 +0100

    progressing

---
+++ GreenCorner/blog/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.utils import timezone
+# Create your models here.
+class Post(models.Model):
+    title= models.CharField(max_length=250)
+    slug= models.SlugField(max_length=250)
+    body=models.TextField()
+    publish=models.DateTimeField(default=timezone.now)
+    created= models.DateTimeField(auto_now_add=True)
+    updated=models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering=['-published'],
+        indexes=[models.Index(fields=['-publish']),]
+
+
+    def __str__(self):
+        return  self.title
